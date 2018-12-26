7eb5f5a49d21f795cdffb5b26cf9098452588f99
Julián Corso <corsojulian7@gmail.com>
2018-12-26 16:27:22 -0300

    [MOD] Change client to publish single

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
 import os
 
 #Enable Logs
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-
-#Init MQTT Client
-client = mqtt.Client("TelegramBot")
-client.username_pw_set(os.getenv('MQTT_USER'), password=os.getenv('MQTT_PASSWORD'))
-client.connect('m15.cloudmqtt.com', port=12048)
 
 #Init bot
 updater = Updater(token=os.getenv('TOKEN'))
@@ -19,14 +14,9 @@ def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="Miau!")
 
 def mqtth(bot, update):
-    cmds = update.message.text.split(' ')
-    rta = 'Error en la publicacion'
-    if len(cmds) == 3:
-        client.publish(cmds[1],cmds[2])
-        rta = 'Publicado correctamente'
-    else:
-        rta = 'Parametros incorrectos'
-    bot.send_message(chat_id=update.message.chat_id, text=rta)
+    auth = {'username':os.getenv('MQTT_USER'), 'password':os.getenv('MQTT_PASSWORD')}
+    publish.single(topic='esp/test', payload=update.message.text[6:], qos=0, hostname='m15.cloudmqtt.com', port=12048, client_id='TelegramBot', auth=auth)
+    bot.send_message(chat_id=update.message.chat_id, text='Publicado correctamente')
 
 def echo(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
