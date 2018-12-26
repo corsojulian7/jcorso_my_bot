e2be7cfead16ee8a20e26610657efd6d91ea3a35
Julián Corso <corsojulian7@gmail.com>
2018-12-25 22:55:20 -0300

    [MOD] Add mqtt command

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import paho.mqtt.client as mqtt
 import os
 
 #Enable Logs
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+#Init MQTT Client
+client = mqtt.Client("TelegramBot")
+client.username_pw_set(os.getenv('MQTT_USER'), password=os.getenv('MQTT_PASSWORD'))
+client.connect('m15.cloudmqtt.com', port=12048)
 
 #Init bot
 updater = Updater(token=os.getenv('TOKEN'))
@@ -12,11 +18,24 @@ dispatcher = updater.dispatcher
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="Miau!")
 
+def mqtth(bot, update):
+    cmds = update.message.text.split(' ')
+    rta = 'Error en la publicacion'
+    if len(cmds) == 3:
+        client.publish(cmds[1],cmds[2])
+        rta = 'Publicado correctamente'
+    else:
+        rta = 'Parametros incorrectos'
+    bot.send_message(chat_id=update.message.chat_id, text=rta)
+
 def echo(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
+
+mqtt_handler = CommandHandler('mqtt', mqtth)
+dispatcher.add_handler(mqtt_handler)
 
 echo_handler = MessageHandler(Filters.text, echo)
 dispatcher.add_handler(echo_handler)
